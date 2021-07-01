fb2c773a1a3c1e20addef00c5068448ceeca5c95
fawolfmann <fawolfmann@gmail.com>
2021-06-30 23:38:01 -0300

    add constants file

---
+++ bert_extractor/constants.py
@@ -0,0 +1,22 @@
+"""Constants file"""
+
+# NER
+NER_LABLES_MAP = {
+    "O": 0,
+    "B-LOC": 1,
+    "B-PER": 2,
+    "I-PER": 3,
+    "I-LOC": 4,
+    "B-MISC": 5,
+    "I-MISC": 6,
+    "B-ORG": 7,
+    "I-ORG": 8,
+}
+NER_KAGGLE_DATASET = {"CoNLL003": "alaakhaled/conll003-englishversion"}
+
+# AMAZON REVIEWS
+REVIEWS_DATASET = {
+    "fashion": "http://deepyeti.ucsd.edu/jianmo/amazon/categoryFilesSmall/AMAZON_FASHION_5.json.gz",
+    "beauty": "http://deepyeti.ucsd.edu/jianmo/amazon/categoryFilesSmall/All_Beauty_5.json.gz",
+    "appliances": "http://deepyeti.ucsd.edu/jianmo/amazon/categoryFilesSmall/Appliances_5.json.gz",
+}
